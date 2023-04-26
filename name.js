6a4e790199ea596301dbe3438c6d58a2c0098886
Becca Stucki <44650994+rebeccaanns@users.noreply.github.com>
2023-04-25 22:13:02 -0600

    mongodb

--- name.js
+++ name.js
@@ -1,3 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gqisnlm.mongodb.net/?retryWrites=true&w=majority";
+
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
